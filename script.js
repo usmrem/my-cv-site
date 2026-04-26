96aaf1ff82c605c9518ce776ac6a224022513e1c
Emir <107417506+usmrem@users.noreply.github.com>
2026-04-27 00:03:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// DOTS ANIMATION
 const dots = document.getElementById("dots");
 const frames = ["", ".", "..", "..."];
 let i = 0;
@@ -6,3 +7,39 @@ setInterval(() => {
   dots.textContent = frames[i];
   i = (i + 1) % frames.length;
 }, 400);
+
+
+// OBSERVER OPTIONS
+const options = {
+  threshold: 0.2
+};
+
+const sections = document.querySelectorAll("section");
+
+const sectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      sectionObserver.unobserve(entry.target);
+    }
+  });
+}, options);
+
+sections.forEach(section => {
+  sectionObserver.observe(section);
+});
+
+const cards = document.querySelectorAll(".project-card");
+
+const cardObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      cardObserver.unobserve(entry.target); // остановить наблюдение
+    }
+  });
+}, options);
+
+cards.forEach(card => {
+  cardObserver.observe(card);
+});
