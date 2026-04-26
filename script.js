2dcac82cf545ce4a68c957b8f5903dbd54f38f6f
Emir <107417506+usmrem@users.noreply.github.com>
2026-04-26 23:06:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,3 @@ setInterval(() => {
   dots.textContent = frames[i];
   i = (i + 1) % frames.length;
 }, 400);
-
-// disable copy
-document.addEventListener("copy", e => e.preventDefault());
-
-// disable right click
-document.addEventListener("contextmenu", e => e.preventDefault());
-
-// disable Ctrl+C
-document.addEventListener("keydown", e => {
-  if (e.ctrlKey && e.key.toLowerCase() === "c") {
-    e.preventDefault();
-  }
-});
